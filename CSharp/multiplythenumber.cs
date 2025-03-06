3173793435805fa99111581b2e75bce3a73e9076
Jordan Hunt <jordan.hunt@g.austincc.edu>
2025-03-06 07:48:19 -0600

    First soultion
    First of many

---
+++ CSharp/multiplythenumber.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Kata
+{
+    public static int Multiply(int number)
+    {
+        int numberOfDigits = 0;
+        if (number < 0)
+        {
+          numberOfDigits = number.ToString().Length - 1;
+        }
+        else
+        {
+          numberOfDigits = number.ToString().Length;
+        }
+        int product = number * (int)Math.Pow(5, numberOfDigits);
+        return product;
+    }
+}
